53a0d2c2d8f915b439b4046580fc7d212b1ec978
Zviad Kvaratskhelia <zviad.kvara000@gmail.com>
2025-01-22 15:50:13 +0400

    Untrack binary_to_text.py from repository

--- tasks/!!!dict_interactive.py
+++
@@ -1,9 +0,0 @@
-# Interactive dictionary 7kyu  CLASSES
-
-# In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
-# # >>> d = Dictionary()
-# >>> d.newentry('Apple', 'A fruit that grows on trees')
-# >>> print(d.look('Apple'))
-# A fruit that grows on trees
-# >>> print(d.look('Banana'))
-# Can't find entry for Banana
